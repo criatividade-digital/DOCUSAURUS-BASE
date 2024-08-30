e1b00e1c0c0b5919b24409fdad4cfcd08c7da80f
APRENDER.digital <info@aprender.digital>
2024-08-30 17:40:25 -0300

    ajustes no template

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,62 +2,39 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
+const divStyle = {
+  width: '100%',
+  display: 'inline-block',
+  textAlign: 'center'
+};
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+const paragrafo = {
+  marginLeft: '20px', /* Define a margem esquerda como 20 pixels */
+  marginRight:'20px', /* Define a margem direita como 20 pixels */
+  maxWidth: '640px',
+  margin: '0 auto',
+  padding: '0 30px 0 30px'
 }
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+      <div className="container" style={paragrafo}>
+        {/* Adicionando o novo recurso "Alo mundo" diretamente */}
+        <div className="row" >
+            <p>Olá! Sou Parahuari Branco, especialista em tecnologia e educação. 
+            Quero dar boas-vindas ao <b>IA Estratégica: <i>guia essencial para CEOs e Líderes</i></b>. 
+            Meu objetivo com este guia é desmistificar a Inteligência Artificial, de forma objetiva, acessível e abrangente, 
+            demonstrando como ela pode se tornar uma peça fundamental na estratégia da sua organização.</p>
+            <p>Explore todo o conteúdo do guia <b><a href="docs/intro">navegando de forma sequencial clicando aqui</a></b> ou utilizando o diagrama com os principais tópicos, apresentado a seguir.
+            </p>
         </div>
+        <center>
+          <div className="row" style={divStyle}>
+              <br />
+          </div>
+          <b>IMAGEM ROTEIRO DO GUIA</b>
+        </center>
       </div>
     </section>
   );
---
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import User from '@site/src/components/NavbarItems/user';
+
+export default {
+  ...ComponentTypes,
+  'custom-Login': User,
+};
